2ce9b36431c77ee3f13d878ff4018b5cb75ad0bd
vanithasanju <55209233+vanithasanju@users.noreply.github.com>
2020-02-18 13:55:09 -0600

    Update git.java

--- Jenkinstogit/src/git.java
+++ Jenkinstogit/src/git.java
@@ -2,7 +2,7 @@
 public class git {
 	 public static void main(String[] args) {
 	        
-	      int num1 = 5, num2 = 15, sum;
+	      int num1 = 5, num2 = 35, sum;
 	      sum = num1 + num2;
 
 	      System.out.println("Sum of these two numbers: "+sum);
